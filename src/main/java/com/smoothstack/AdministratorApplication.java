55edb1772ac48a4632c660124ecd4a885b3c2071
VanceaV <vancea.vladimir@gmail.com>
2019-03-27 10:25:12 -0400

    first commit

---
+++ src/main/java/com/smoothstack/AdministratorApplication.java
@@ -0,0 +1,14 @@
+package com.smoothstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AdministratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministratorApplication.class, args);
+	}
+
+}
